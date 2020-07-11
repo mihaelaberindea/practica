0e91a1c4cbe3604fc64fcdaafc71e3c26e68fb64
root <root@DESKTOP-THV13T4.localdomain>
2020-07-11 20:13:35 +0300

    edit main

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
-#include "List.hpp"
 #include "Vector.hpp"
-using namespace std;
+#include "List.hpp"
+#include "ListNode.hpp"
+#include <cstdlib>
 
-int main()
-{
-  
+int main(){
+    Vector vec;
+    std::size_t end = vec.getCapacity();
 
-} 
+    for (std::size_t idx = 0; idx < end; ++idx){
+        vec.setElement(idx, std::rand());
+    }
+
+    int sum = 0;
+
+    for (std::size_t idx = 0; idx < 0; ++idx){
+        sum += vec.getElement(idx);
+    }
+   return sum;
+}
