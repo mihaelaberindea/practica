51b490fcb5c0dd5eec52580a5bea8b8641d18edf
root <root@DESKTOP-THV13T4.localdomain>
2020-07-11 21:28:16 +0300

    second diagram

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,20 @@
 #include <cstdlib>
 
 int main(){
-    Vector vec;
-    std::size_t end = vec.getCapacity();
+      Vector<int> vec;
+ 
+  vec.pushBack(0);
+  vec.pushBack(1);
+  vec.pushBack(2);
+  vec.pushBack(3);
+  vec.pushBack(4);
+  vec.pushFront(5);
 
-    for (std::size_t idx = 0; idx < end; ++idx){
-        vec.setElement(idx, std::rand());
-    }
+  Vector<int> vec2 = vec; //copy constructor
+  vec.print();
+  std::cout<< vec.getFront() << " " << vec.getBack() << " " << vec.getElement(1) <<" " << std::endl;
 
-    int sum = 0;
-
-    for (std::size_t idx = 0; idx < 0; ++idx){
-        sum += vec.getElement(idx);
-    }
-   return sum;
+  std::cout << vec.getCapacity() << std::endl;
+  vec.reserve(15);
+  std::cout << vec.getCapacity() << std::endl;
 }
