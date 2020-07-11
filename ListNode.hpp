b179a6935e59127976982e836a51aac4b113672f
root <root@DESKTOP-THV13T4.localdomain>
2020-07-11 21:55:57 +0300

    second diagram

--- ListNode.hpp
+++ ListNode.hpp
@@ -1,19 +1,21 @@
 #include <cstddef>
+template <typename T>
 class ListNode
 {
     public:
-    ListNode ( ListNode* prev, ListNode* next, int data);
+    ListNode ( ListNode* prev, ListNode* next,  T data);
+    ListNode (const ListNode& rhs)
    ~ListNode();
     ListNode* getPrev();
      ListNode* getNext();
     void setPrev(ListNode* prev);
     void setNext(ListNode* next);
-   int getData();
-   void setData(ListNode* data);
+     T getData();
+   void setData(T data);
     private:
     ListNode* m_prev;
     ListNode* m_next;
-    int m_data;
+    T m_data;
 
     
 };
