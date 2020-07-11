cff50bb492ed1660b582dd3f0bde435049335d3a
root <root@DESKTOP-THV13T4.localdomain>
2020-07-11 21:27:15 +0300

    Changes second diagram

--- Vector.hpp
+++ Vector.hpp
@@ -8,24 +8,25 @@ class Vector
     public:
     std::size_t getSize();
     std::size_t getCapacity();
-    void insert( std::size_t idx, Tvalue element);
-    int pushFront(Tvalue element);
-    int pushBack(Tvalue element);
+    void insert( std::size_t idx, T element);
+    int pushFront(T element);
+    int pushBack(T element);
     Tvalue getElement(std::size_t idx);
     void erase (std::size_t idx);
     Tvalue getFront();
     Tvalue getBack();
-    int  setElement(std::size_t idx, Tvalue element);
-    int setFront(Tvalue element);
-    int setBack(Tvalue element);
+    int  setElement(std::size_t idx, T element);
+    int setFront(T element);
+    int setBack(T element);
     void clear();
     bool empty();
 
     private:
     std::size_t m_capacity;
     std::size_t m_size;
-    Tvalue m_data;
+    T m_data;
     void reserve(std::size_t newCapacity);
     
 
 };;
+#include "Vector.tpp"
