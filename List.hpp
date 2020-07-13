9080933323972226044ad92a943d69407ff0b3ab
root <root@DESKTOP-THV13T4.localdomain>
2020-07-13 13:56:45 +0300

    second diagram

--- List.hpp
+++ List.hpp
@@ -1,28 +1,34 @@
 #pragma once
 #include <cstddef>
 #include "ListNode.hpp"
+template<typename T>
  class List 
  {
      public:
       List();
+      List(const ListNode& rhs);
       ~List();
     std:: size_t getSize();
-    void insert(std::size_t idx , int element);
-    void pushFront(int element);
-    void pushBack(int element);
-    int getElement(std::size_t idx);
+    void insert(std::size_t idx , T element);
+    void pushFront(T element);
+    void pushBack(T element);
+    T getElement(std::size_t idx);
     int getFront();
-    int getBack();
-    void setElement(std::size_t idx, int element);
-    int setFront(int element);
-    int setBack(int element);
+    T getBack();
+    void erase(std::size_t idx)
+    void popFront();
+    void popBack();
+    void setElement(std::size_t idx, T element);
+    void setFront(T element);
+    void setBack(T element);
     void clear();
     bool empty();
 
     private:
-    std::size_t m_size;
+    size_t m_size;
     ListNode* m_head;
     ListNode* m_tail;
 
 
  };
+ #include "List.tpp"
